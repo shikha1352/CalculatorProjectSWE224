e8f96468bac06be3a4081f247e8fe3c0ec2be95a
shikha1352 <pnsshikha@gmail.com>
2024-05-30 11:12:01 +0600

    Initial commit

---
+++ src/CalculatorApplication/Main.java
@@ -0,0 +1,8 @@
+package CalculatorApplication;
+public class Main {
+    public static void main(String[] args) {
+        CalculatorApplication calculator=new CalculatorApplication();
+
+    }
+
+}
